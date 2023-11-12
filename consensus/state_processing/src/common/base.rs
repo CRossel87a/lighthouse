2be634116d208259cd61ed7ce58794a7b02ccbb1
CRossel87a <chris.rossel@gmail.com>
2023-11-12 23:51:48 +0100

    abc

--- consensus/state_processing/src/common/base.rs
+++ consensus/state_processing/src/common/base.rs
@@ -20,12 +20,18 @@ pub fn get_base_reward<T: EthSpec>(
 
 pub fn get_base_reward_from_effective_balance<T: EthSpec>(
     effective_balance: u64,
-    total_active_balance: u64,
+    total_active_balance: u128,
     spec: &ChainSpec,
 ) -> Result<u64, BeaconStateError> {
-    effective_balance
+
+    (((effective_balance as u128).safe_mul(spec.base_reward_factor as u128)?.safe_div(total_active_balance.integer_sqrt())?) as u64).safe_div(spec.base_rewards_per_epoch).map_err(Into::into)
+
+    /* 
+    ((effective_balance
         .safe_mul(spec.base_reward_factor)?
-        .safe_div(total_active_balance.integer_sqrt())?
+        .safe_div(total_active_balance.integer_sqrt())?) as u64)
         .safe_div(spec.base_rewards_per_epoch)
         .map_err(Into::into)
+
+    */
 }
